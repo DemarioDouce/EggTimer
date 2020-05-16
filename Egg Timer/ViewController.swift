b1aa1fb8989ef19f09bcbbd8eceffdefe196946c
Demario Douce <demario.douce567@gmail.com>
2020-05-16 17:41:59 -0400

    Initial Commit

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Demario Douce on 2020-05-16.
+//  Copyright © 2020 Demario Douce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
